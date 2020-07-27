6d72bcdd9d8aebec93a9daaaf4fe4112eeead73b
Artur <54365622+bunge12@users.noreply.github.com>
2020-07-27 16:32:21 -0400

    deleteEvent function added

---
+++ script.js
@@ -0,0 +1,25 @@
+// Deletes selected record
+const deleteEvent = (eventDetails) => {
+  const { week, day, hour, length } = eventDetails;
+  for (let i = 0; i <= length; i++) {
+    schedule[week].weekdays[day].schedule[parseInt(hour) + i].task = [];
+  }
+};
+
+// Saves an event
+const saveEvent = (eventDetails) => {
+  const { task, details, location, length, week, day, hour } = eventDetails;
+  let correctedLength = parseInt(length) + 1;
+  for (let i = 0; i < correctedLength; i++) {
+    schedule[week].weekdays[day].schedule[parseInt(hour) + i].task = {
+      job: task,
+      details: details,
+      location: location,
+    };
+  }
+};
+
+// Cleans up text from a form
+const cleanup = (text) => {
+  return text.split("=")[1].trim();
+};
